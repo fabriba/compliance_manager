a35604a5da5d83294f67e06198c5d18ac7fb6049
Fa <fabriba@users.noreply.github.com>
2026-02-25 19:39:09 +0100

    Add constants for compliance manager integration
    Initialized constants for compliance manager integration.

---
+++ const.py
@@ -0,0 +1,21 @@
+from datetime import timedelta
+
+
+TESTMODE = True
+# Integration Domain
+DOMAIN = "compliance_manager"  # Change this to your actual folder name
+
+
+SNOOZE_ATTRIBUTE = "snoozed"
+GRACE_ATTRIBUTE = "grace_periods"
+SEVERITY_LEVELS = {
+    "critical": 0,
+    "problem": 1,
+    "warning": 2,
+    "unusual": 3,
+    "info": 4,
+}
+DEFAULT_SEVERITY = "problem"
+DEFAULT_ICON = "mdi:shield-check"
+DEFAULT_GRACE = timedelta(seconds=0)
+
