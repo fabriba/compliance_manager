7ee76b0dfa60288addd115d11d0a2bbebc73aa6a
Fa <fabriba@users.noreply.github.com>
2026-02-26 17:24:22 +0100

    Add LAB and DEBUG variables to const.py

--- const.py
+++ const.py
@@ -1,10 +1,14 @@
 from datetime import timedelta
 
-
+# LAB and DEBUG variables
 TESTMODE = True
+NUM_TEST_GROUPS = 4
+LAB_PREFIX = "switch.compliance_manager_lab_tester_"
+SHOW_DEBUG_ATTRIBUTES = True
+
 # Integration Domain
 DOMAIN = "compliance_manager"  # Change this to your actual folder name
-
+PLATFORMS = ["binary_sensor"] if not TESTMODE else ["binary_sensor", "switch"]
 
 SNOOZE_ATTRIBUTE = "snoozed"
 GRACE_ATTRIBUTE = "grace_periods"
@@ -18,4 +22,5 @@ SEVERITY_LEVELS = {
 DEFAULT_SEVERITY = "problem"
 DEFAULT_ICON = "mdi:shield-check"
 DEFAULT_GRACE = timedelta(seconds=0)
+
 
