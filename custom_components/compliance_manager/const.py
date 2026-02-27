646cd1231021d786bfca5980c4842e78497ff884
Fa <fabriba@users.noreply.github.com>
2026-02-27 11:55:28 +0100

    Update const.py

--- custom_components/compliance_manager/const.py
+++ custom_components/compliance_manager/const.py
@@ -1,14 +1,11 @@
 from datetime import timedelta
 
 # LAB and DEBUG variables
-TESTMODE = False
-NUM_TEST_GROUPS = 0
 LAB_PREFIX = "switch.compliance_manager_lab_tester_"
-SHOW_DEBUG_ATTRIBUTES = False
 
 # Integration Domain
 DOMAIN = "compliance_manager"  # Change this to your actual folder name
-PLATFORMS = ["binary_sensor"] if not TESTMODE else ["binary_sensor", "switch"]
+PLATFORMS = ["binary_sensor", "switch"]
 
 SNOOZE_ATTRIBUTE = "snoozed"
 GRACE_ATTRIBUTE = "grace_periods"
